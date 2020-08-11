89e05ffe8f29a3138353d2c631a4dbe0e7104286
farajalboriny <40642240+farajalboriny@users.noreply.github.com>
2020-08-12 01:28:14 +0300

    Update register.service.ts

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -11,6 +11,6 @@ export class RegisterService {
   constructor(private httpClient:HttpClient) { }
 
   register(user:User):Observable<User> {
-    return this.httpClient.post<User>('http://3.129.208.173:8000/register',user);
+    return this.httpClient.post<User>('http://backend-592385535.us-east-2.elb.amazonaws.com/register',user);
   }
 }
