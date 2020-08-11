a63ebc4cde643da598326a2dbeeb516c301eadd0
farajalboriny <40642240+farajalboriny@users.noreply.github.com>
2020-08-12 01:48:03 +0300

    Update login.service.ts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,6 +10,6 @@ export class LoginService {
 
   constructor(private httpClient:HttpClient) { }
   public login(user:User):Observable<User>{
-  return this.httpClient.post<User>('http://backend-592385535.us-east-2.elb.amazonaws.com/login',user);
+  return this.httpClient.post<User>('http://3.129.208.173:8000/login',user);
   }
 }
